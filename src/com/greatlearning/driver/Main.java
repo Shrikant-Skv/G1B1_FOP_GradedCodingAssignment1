b7f2245bd75a863869417d1037979ce62232eb1e
Shrikant Verma <123170814+Shrikant-Skv@users.noreply.github.com>
2023-03-13 21:29:10 +0530

    Add files via upload

---
+++ src/com/greatlearning/driver/Main.java
@@ -0,0 +1,34 @@
+package com.greatlearning.driver;
+//Driver Class
+
+import com.greatlearning.departments.AdminDepartment;
+import com.greatlearning.departments.HRDepartment;
+import com.greatlearning.departments.TechDepartment;
+
+public class Main {
+	public static void main(String[] args) {
+		AdminDepartment admin = new AdminDepartment();
+		HRDepartment hr = new HRDepartment();
+		TechDepartment tech = new TechDepartment();
+
+		System.out.println("Welcome to " + admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday());
+		System.out.println();
+
+		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.println();
+
+		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+		System.out.println();
+	}
+}
